269e0a12502d144f288e7bde61cd83887aa765ea
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-01-05 19:26:03 +0000

    add tailwind css colour

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Header } from 'components/shared/header'
 import 'styles/globals.scss'
 
-// Todo: change BG-class to tailwind class
 export default function RootLayout({
   children,
 }: {
@@ -14,7 +13,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='bg-[#152233] '>
+      <body className='bg-brandBackground'>
         <Header />
         {children}
       </body>
