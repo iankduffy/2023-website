c9bd2493771fe18a6789cc525c91cf0b21746425
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-01-05 19:13:51 +0000

    improve flex on navigation

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,14 +1,13 @@
 
-// Todo: Redo this in grid
-export function Header({}) {
+// Question: Could this be better in Grid
+export function Header({}): JSX.Element {
   return (
     <header>
       <div className='container mx-auto flex items-center justify-between px-3 py-5 text-white gap-5'>
-        {/* Navigation */}
         <nav className='w-1/5 lg:order-2 lg:w-full lg:text-center'>Navigation</nav>
-        {/* Logo */}
-        <img src="/logo.svg" width="50" height="50" alt="Ian Duffy Logo" className=''/>
-        {/* Socials */}
+        <div className='lg:w-1/5'>
+          <img src="/logo.svg" width="50" height="50" alt="Ian Duffy Logo" className=''/>
+        </div>
         <div className='w-1/5 lg:order-3 text-right'>Socials / Github</div>
       </div>
     </header>
