67fade618208059fb792972ed4d934e77ad2a437
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-01-21 19:43:53 +0000

    set up front end link to sanity

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,25 @@ import { AboutSection } from "components/homepage/about";
 import { Hero } from "components/homepage/hero";
 import { ProjectsShowcase } from "components/homepage/projects";
 import { SkillsSection } from "components/homepage/skills";
+import { clientFetch } from "lib/sanity";
 
-export default function Home() {
+export async function generateStaticParams() {
+  const genericPage = await getData();
+
+  return genericPage.map((page: { slug: { current: string; }; }) => ({
+    slug: page.slug.current,
+  }));
+}
+
+const getData = async () => {
+  const pageContent = await clientFetch('*[_type == "genericPages"]')
+
+  return pageContent
+}
+
+export default async function Home() {
+  const data = await getData()
+
   return (
     <main>
       <Hero />
