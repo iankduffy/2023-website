0324e7454c63b5337400f003df23214aeb951dcb
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-01-30 20:32:21 +0000

    next-13 work around

--- frontend/components/articles/content/blocks/headings/index.tsx
+++ frontend/components/articles/content/blocks/headings/index.tsx
@@ -12,6 +12,9 @@ const headingClasses: HeadingProps = {
   h4: 'px-4 my-2 text-xl font-bold lg:px-0',
 }
 
+// Next13 App currently has a issue with linking to hash
+// Once fix this will be disabled.
+const NEXT_LINK_13_BROKEN = true
 export const Heading2: PortableTextBlockComponent = function ({ children, value }) {
   const { style, _key } = value
 
@@ -20,11 +23,21 @@ export const Heading2: PortableTextBlockComponent = function ({ children, value 
 
   if (!HeadingTag) return null
 
+  if (NEXT_LINK_13_BROKEN) {
+    return (
+      <a href={`#${headingId}`}>
+        <HeadingTag className={headingClasses[HeadingTag]} id={headingId}>
+          {children}
+        </HeadingTag>
+      </a>
+    )
+  }
+
   return (
     <Link href={`#${headingId}`} shallow={true} scroll={true} id={headingId}>
-      <p className={headingClasses[HeadingTag]}>
+      <HeadingTag className={headingClasses[HeadingTag]}>
         {children}
-      </p>
+      </HeadingTag>
     </Link>
   )
 }
