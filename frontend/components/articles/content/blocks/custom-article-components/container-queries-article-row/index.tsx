ba16a2e37bbcf4b7808ed4d2dac4cec35b6cee64
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-02-02 14:07:45 +0000

    basic article component

--- frontend/components/articles/content/blocks/custom-article-components/container-queries-article-row/index.tsx
+++ frontend/components/articles/content/blocks/custom-article-components/container-queries-article-row/index.tsx
@@ -8,11 +8,11 @@ function ArticleCard() {
     <article className={`${styles.container}`}>
       <div className={styles.article}>
         <img className="object-cover" loading="lazy" alt="Article Image" src="https://images.unsplash.com/photo-1591491719565-9cae8fd89f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80&h=1000" width="400" height="500" />
-        <div className="p-4">
-          <p>Category</p>
-          <p>Super Long Heading</p>
-          <button>Read</button>
-          <p>5 Min Read</p>
+        <div className="flex flex-col items-start gap-2 p-4">
+          <p className="block w-full py-2 text-sm border-b">Category</p>
+          <p className="text-xl font-bold">Super Long Heading</p>
+          <button className="px-4 py-1 text-white bg-black">Read</button>
+          <p className="text-sm">5 Min Read</p>
         </div>
       </div>
     </article>
@@ -30,9 +30,9 @@ export function ArticleContainerQueriesRow() {
         )}
       </ul>
       {/* Article Row */}
-      <div className='flex justify-center gap-5'>
-        <button onClick={() => setArticleNumber(curr => curr + 1)} disabled={articleNumber >= 3 ? true : false}>Add Item</button>
-        <button onClick={() => setArticleNumber(curr => curr - 1)} disabled={articleNumber <= 1 ? true : false}>Remove Item</button>
+      <div className='flex justify-center gap-5 my-5'>
+        <button className='px-4 py-2 text-white bg-brandLight' onClick={() => setArticleNumber(curr => curr + 1)} disabled={articleNumber >= 3 ? true : false}>Add Item</button>
+        <button className='px-4 py-2 text-white bg-brandLight' onClick={() => setArticleNumber(curr => curr - 1)} disabled={articleNumber <= 1 ? true : false}>Remove Item</button>
       </div>
     </section >
   )
