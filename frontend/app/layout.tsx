77b539d5f8bd36823172c7afd1364b4f12d1befb
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-02-07 16:11:11 +0000

    no-js stop animation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from 'components/shared/header'
+import Script from 'next/script'
 import 'styles/globals.scss'
 
 export default function RootLayout({
@@ -13,10 +14,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='bg-brandBackground'>
+      <body className='bg-brandBackground no-js'>
         <Header />
         {children}
       </body>
+      <Script>{`document.querySelector('body').classList.remove('no-js')`}</Script>
     </html>
   )
 }
