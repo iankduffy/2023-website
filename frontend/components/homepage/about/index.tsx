e3fb00639c826dcebdb5ffbc5b2862e955d17a2b
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-02-07 09:30:46 +0000

    improve delay

--- frontend/components/homepage/about/index.tsx
+++ frontend/components/homepage/about/index.tsx
@@ -20,12 +20,12 @@ export function AboutSection({ title = '', content = [], image }: Props): JSX.El
     <AnimateIn>
       <section className='container grid items-center min-h-screen gap-10 py-5 mx-auto lg:grid-cols-2'>
         <div className='px-3 lg:mb-10'>
-          <h2 className='my-2 mb-4 text-3xl font-bold text-white uppercase lg:text-5xl' style={{ '--delay': `${500}ms` } as React.CSSProperties}>{title}</h2>
-          <div className='text-light [&>*]:mb-4' style={{ '--delay': `${700}ms` } as React.CSSProperties}>
+          <h2 className='my-2 mb-4 text-3xl font-bold text-white uppercase lg:text-5xl' style={{ '--delay': `${100}ms` } as React.CSSProperties}>{title}</h2>
+          <div className='text-light [&>*]:mb-4' style={{ '--delay': `${200}ms` } as React.CSSProperties}>
             <PortableText value={content} />
           </div>
         </div>
-        <img src={url} loading='lazy' width='500' height='500' alt='me' style={{ '--delay': `${900}ms` } as React.CSSProperties} />
+        <img src={url} loading='lazy' width='500' height='500' alt='me' style={{ '--delay': `${400}ms` } as React.CSSProperties} />
       </section>
     </AnimateIn>
 
