ad3d087222830af1c15016352392dbc90b90b12f
Ian Duffy <ian.duffy@shiftcommerce.com>
2023-02-12 14:12:52 +0000

    add logo

--- studio/plugins/cv/aside.tsx
+++ studio/plugins/cv/aside.tsx
@@ -1,12 +1,15 @@
 import { CvBlock } from './blocks'
 import styles from './styles.module.scss'
-
+import { urlFor } from '../../../frontend/lib/sanity'
 export function AsideCV({ aside }) {
   const { logo, name, jobTitle, links, blocks } = aside
-  console.log(aside)
+
+  // const image = urlFor(logo).url()
+  // console.log(image)
   return (
     <div>
       <div className={styles.topSection}>
+        <img src='https://www.iankduffy.com/logo.svg' className={styles.logo} loading='eager' />
         <h1>{name}</h1>
         <p>{jobTitle}</p>
       </div>
