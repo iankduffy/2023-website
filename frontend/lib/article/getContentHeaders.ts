2dbbb9b0ecc94ef2d90b8ea7a62c9fc9775e0668
iankduffy <i.duffy7999@gmail.com>
2023-02-16 11:55:19 +0000

    optional chaining

--- frontend/lib/article/getContentHeaders.ts
+++ frontend/lib/article/getContentHeaders.ts
@@ -1,7 +1,7 @@
 import { PortableTextBlock } from '@portabletext/types';
 
 export function getContentHeader(content: PortableTextBlock[]) {
-  return content.filter(
+  return content?.filter(
     (block) => typeof block.style === 'string' && /^h\d/.test(block.style)
   );
 }
