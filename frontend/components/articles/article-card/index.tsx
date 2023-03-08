94b7a707319558aecf881e9ef13e661503e5362d
iankduffy <i.duffy7999@gmail.com>
2023-03-08 13:28:06 +0000

    update publish date

--- frontend/components/articles/article-card/index.tsx
+++ frontend/components/articles/article-card/index.tsx
@@ -9,14 +9,14 @@ interface Props {
   estimatedReadingTime: number | null
 }
 
-export function ArticleCard({ title, description = '', slug, _createdAt, estimatedReadingTime }: Props) {
-  const publishedDate = new Date(_createdAt).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })
+export function ArticleCard({ title, description = '', slug, publishedDate, estimatedReadingTime }: Props) {
+  const date = new Date(publishedDate).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })
 
   return (
     <Link href={`/articles/${slug.current}`} className='block py-4 not-last:border-b border-light'>
       <h3 className="mb-2 text-2xl font-bold">{title}</h3>
       <div className="flex gap-6 font-bold uppercase text-light">
-        <p>{publishedDate}</p>
+        <p>{date}</p>
         <p>{estimatedReadingTime} mins read</p>
       </div>
       <p className='my-2 text-lg text-light'>{description}</p>
