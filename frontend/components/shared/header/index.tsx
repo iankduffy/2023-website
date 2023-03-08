ba8a5e497c6bfb95630ff59e35a3d6af78fe41b1
iankduffy <i.duffy7999@gmail.com>
2023-03-08 18:24:12 +0000

    add aria labels

--- frontend/components/shared/header/index.tsx
+++ frontend/components/shared/header/index.tsx
@@ -17,8 +17,8 @@ export async function Header({ }): any {
           <img src="/logo.svg" width="50" height="55" alt="Ian Duffy Logo" loading="eager" fetchPriority="high" className='' />
         </Link>
         <div className="flex gap-2 lg:gap-5 text-large items-center">
-          {linkedin && <Link href={linkedin} target='_blank' className='h-full py-2 text-2xl'><FaLinkedin /><span className='hidden'>Linkedin</span></Link>}
-          {github && <Link href={github} target='_blank' className='h-full py-2 text-2xl'><FaGithub /><span className='hidden'>Github</span></Link>}
+          {linkedin && <Link href={linkedin} target='_blank' className='h-full py-2 text-2xl' aria-label='Linkedin'><FaLinkedin /><span className='hidden'>Linkedin</span></Link>}
+          {github && <Link href={github} target='_blank' className='h-full py-2 text-2xl' aria-label='github'><FaGithub /><span className='hidden'>Github</span></Link>}
           <div className='w-1/5 text-center lg:order-3'>
             <Link href={cv} target='_blank' className='px-10 py-2 bg-[#223854] rounded hover:bg-[#21354F] transition duration-500'>CV</Link>
           </div>
